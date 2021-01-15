f56c3c36ba7b6dcc90dbcbea4ab4b3a77520795a
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-15 16:31:18 -0300

    Fix in deploy

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data '{ "updates": [ { "type": "web", "docker_image": "$IMAGE_ID" } ] }'
