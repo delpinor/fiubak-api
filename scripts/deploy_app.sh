717cbca92b000f03049e925e5c7c1a31bceb57f2
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-16 09:01:08 -0300

    Test to validate idea

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -2,4 +2,4 @@
 
 echo "image id: $IMAGE_ID"
 
-curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data '{ "updates": [ { "type": "web", "docker_image": "$IMAGE_ID" } ] }'
+curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data '{ "updates": [ { "type": "web", "docker_image": "$IMAGE_ID" } ] }' --fail
