9abb65bbbeab632e20c2c44836c8f12549160be6
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-16 10:38:35 -0300

    Deploy ready

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -2,12 +2,8 @@
 
 set -x 
 
-echo "image id: $IMAGE_ID"
-
 DATA='{ "updates": [ { "type": "web", "docker_image":"'
 DATA="$DATA$IMAGE_ID"
 DATA=$DATA'" } ] }'
-
-echo "data: $DATA"
 
 curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data "$DATA" --fail
