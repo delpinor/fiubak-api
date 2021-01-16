893e976f023ec975b078661be307e8fbc2e5e414
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-16 09:11:00 -0300

    Delay added to deploy

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# first wait some seconds to ensure the image is registered bby heroku
+sleep 10
+
 echo "image id: $IMAGE_ID"
 
 curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data '{ "updates": [ { "type": "web", "docker_image": "$IMAGE_ID" } ] }' --fail
