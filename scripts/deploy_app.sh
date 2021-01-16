5302aad27947534ae9f1255f3031148d56ec7bc7
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-16 09:45:29 -0300

    Deploy script updated

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# first wait some seconds to ensure the image is registered bby heroku
-sleep 30
-
 echo "image id: $IMAGE_ID"
 
-curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data '{ "updates": [ { "type": "web", "docker_image": "$IMAGE_ID" } ] }' --fail
+DATA='{ "updates": [ { "type": "web", "docker_image":"'
+DATA="$DATA$IMAGE_ID"
+DATA=$DATA'" } ] }'
+
+echo "data: $DATA"
+
+curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data $DATA --fail
