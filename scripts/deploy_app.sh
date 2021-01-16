5bd506a250decaef9e66552aed00b316f02faa38
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-01-16 10:30:17 -0300

    Try in bash script

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x 
+
 echo "image id: $IMAGE_ID"
 
 DATA='{ "updates": [ { "type": "web", "docker_image":"'
@@ -8,4 +10,4 @@ DATA=$DATA'" } ] }'
 
 echo "data: $DATA"
 
-curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data $DATA --fail
+curl -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data "$DATA" --fail
