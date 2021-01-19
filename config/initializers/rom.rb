cf302e5b01f8230112938da2a45be0c2117526bb
egutter <egutter@gmail.com>
2021-01-19 14:52:59 -0300

    Fix DB config and add docker-compose.prod.yml

--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -1,5 +1,6 @@
 require 'rom'
-DATABASE_URL = ENV['DATABASE_URL']
+puts "in rom.rb DATABASE_URL #{DATABASE_URL}"
+
 DB = ROM.container(:sql, DATABASE_URL) do |config|
   config.relation(:users) do
     schema(infer: true)
