c5444fe84f9fc869893725b528564b5d252ea92e
nicopaez <nicopaez@computer.org>
2021-01-19 15:12:42 -0300

    Issue dabatase

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,11 +3,10 @@
 # Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 #
 
-
-case ENV['RACK_ENV']
-when 'test'
+case ENV['RACK_ENV'].to_sym
+when :test
   DATABASE_URL = ENV['TEST_DB_URL'] || 'postgres://postgres:postgres@localhost/webapi_template_test'
-when 'development'
+when :development
   DATABASE_URL = ENV['DEV_DB_URL'] || 'postgres://postgres:postgres@localhost/webapi_template_development'
 else
   DATABASE_URL = ENV['DATABASE_URL']
--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -1,7 +1,9 @@
 require 'rom'
+
 DB = ROM.container(:sql, DATABASE_URL) do |config|
   config.relation(:users) do
     schema(infer: true)
     auto_struct true
   end
+
 end
