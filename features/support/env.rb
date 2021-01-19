35fd3262a054c55e8d0c4ee124feddaaef90a90d
egutter <egutter@gmail.com>
2021-01-19 18:25:01 -0300

    Run cucumber with embedded Padrino

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,11 +4,12 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../config/boot")
 
 require 'rspec/expectations'
 
+
 if ENV['BASE_URL']
   BASE_URL = ENV['BASE_URL']
 else
+  BASE_URL = 'http://localhost:3000'
   include Rack::Test::Methods
-  BASE_URL='http://localhost:3000'
   def app
     Padrino.application
   end
