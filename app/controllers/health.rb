7ad4625b9614d825606a3a754595a2f7c7f4f9bc
nicopaez <nicopaez@computer.org>
2021-01-24 09:46:03 -0300

    Smoke tests added

---
+++ app/controllers/health.rb
@@ -0,0 +1,17 @@
+WebTemplate::App.controllers :health, :provides => [:json] do
+
+  get :index do
+    status 200
+    logger.info 'status:ok'
+    {
+      :status => 'ok',
+      :version => Version.current
+    }.to_json
+  end
+
+  get :version do
+    status 200
+    { :version => Version.current}.to_json
+  end
+
+end
---
+++ scripts/smoke_test.sh
@@ -0,0 +1,25 @@
+
+#/bin/bash
+
+TARGET_HOST=$1
+VERSION=$2
+
+TARGET_URL="http://$TARGET_HOST/health/version"
+
+i="0"
+
+while [ $i -lt 3 ]
+do
+  echo "Smoke test"
+  result=`curl  $TARGET_URL | grep $VERSION`
+  if [ "$?" = "0" ];
+  then
+  	echo "Test:OK"
+  	exit 0
+  fi
+  i=$(( $i+1 ))
+  sleep 20
+done
+
+echo "Test:FAIL"
+exit 1
