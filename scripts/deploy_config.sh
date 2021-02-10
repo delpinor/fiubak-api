b8fd7f1d6a61d5f747878858f7aa3aefa0be64b0
Nico Paez <nicopaez@computer.org>
2021-02-10 19:08:59 -0300

    Support for configuration deployment

---
+++ scripts/deploy_config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -x
+
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/config-vars -d @${CI_ENVIRONMENT_NAME}.config.json -H "Content-Type: application/json"  -H "Accept: application/vnd.heroku+json; version=3" --header "Authorization: Bearer ${HEROKU_TOKEN}"
