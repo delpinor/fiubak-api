5b9c94710382b4e79f4dd5618fa329d1954b5d0d
nicopaez <nicopaez@computer.org>
2021-02-10 20:49:55 -0300

    Fix in ci script

--- scripts/deploy_config.sh
+++ scripts/deploy_config.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/config-vars -d @${CI_ENVIRONMENT_NAME}.config.json -H "Content-Type: application/json"  -H "Accept: application/vnd.heroku+json; version=3" --header "Authorization: Bearer ${HEROKU_TOKEN}"
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/config-vars -d @scripts/${CI_ENVIRONMENT_NAME}.config.json -H "Content-Type: application/json"  -H "Accept: application/vnd.heroku+json; version=3" --header "Authorization: Bearer ${HEROKU_TOKEN}"
