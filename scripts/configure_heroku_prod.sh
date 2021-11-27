4bc18b5c9251e24c0b85c23ce1aac75d5297dbdc
delpinor <delpinor@gmail.com>
2021-11-27 12:07:46 -0300

    build: actualizacion de scripts de configuracion

--- scripts/configure_heroku_prod.sh
+++ scripts/configure_heroku_prod.sh
@@ -5,7 +5,7 @@ set -e
 
 # heroku login
 
-APP_NAME=webapi-example-test
+APP_NAME=webapi-nairobi-prod
 
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
---
+++ scripts/configure_heroku_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# IMPORTANT: before running this script ensure to run heroku login!!!!
+
+set -e
+
+# heroku login
+
+APP_NAME=webapi-nairobi-test
+
+heroku apps:create $APP_NAME
+heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
+heroku config:set RACK_ENV=production --app $APP_NAME
+heroku drains:add $SUMOLOGIC_SOURCE --app $APP_NAME
