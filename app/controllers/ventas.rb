4c7d65415684c57934fa2c1d862509c8ccc59b1f
Gaston Parente <ggparente95@gmail.com>
2021-11-29 23:53:32 -0300

    refactor: cambiando nombre de url

--- app/controllers/ventas.rb
+++ app/controllers/ventas.rb
@@ -1,9 +1,9 @@
 WebTemplate::App.controllers :usuarios, :provides => [:json] do
 
-  post :create, :map => '/usuarios/:id/ventas' do
+  post :create, :map => '/usuarios/:id/intenciones_de_venta' do
     begin
       status 201
-      {mensaje: 'intencion de venta registrada con exito'}.to_json
+      {mensaje: 'intencion de venta registrada con exito', id: 1}.to_json
     end
   end
 
--- spec/app/controllers/ventas_controller_spec.rb
+++ spec/app/controllers/ventas_controller_spec.rb
@@ -5,9 +5,10 @@ describe 'Ventas controller' do
   let(:datos_auto) { {marca: "Mercedes", modelo: "2322", anio: 2018, patente: "MHF200"} }
 
   it 'La respuesta debe ser un mensaje exitoso' do
-    post('/usuarios/1/ventas', datos_auto.to_json, { 'CONTENT_TYPE' => 'application/json' })
+    post('/usuarios/1/intenciones_de_venta', datos_auto.to_json, { 'CONTENT_TYPE' => 'application/json' })
     body = JSON.parse(last_response.body)
     expect(body['mensaje']).to eq('intencion de venta registrada con exito')
+    expect(body['id']).to eq(1)
   end
 
 end
