6d350c25801e35357327696262cdab1539f13099
lotero1 <lotero@fi.uba.ar>
2021-11-30 20:18:05 -0300

    refactor: Agrega modelo de output de consulta de estados para no devolver una clase del dominio
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

--- app/helpers/ventas_helper.rb
+++ app/helpers/ventas_helper.rb
@@ -21,7 +21,9 @@ module WebTemplate
       end
 
       def recuperar_intenciones_de_venta(id_usuario)
-        return repositorio_de_intencion_de_ventas.encontrar_por_id_usuario(id_usuario)
+        return repositorio_de_intencion_de_ventas.encontrar_por_id_usuario(id_usuario).map {
+           |intencion_de_venta| ResultadoConsultaIntencionDeVenta.new(intencion_de_venta.auto, intencion_de_venta.estado, intencion_de_venta.id) 
+          }
       end
 
     end
