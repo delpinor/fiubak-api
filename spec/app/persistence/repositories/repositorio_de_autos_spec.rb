d9d84d49c6b3df48d51938e0e2358a57f8f1161a
Gaston Parente <ggparente95@gmail.com>
2021-11-30 00:47:21 -0300

    test: Repositorio de autos pasando los tests

--- spec/app/persistence/repositories/repositorio_de_autos_spec.rb
+++ spec/app/persistence/repositories/repositorio_de_autos_spec.rb
@@ -13,4 +13,31 @@ describe Persistence::Repositories::RepositorioDeAutos do
     expect(repo_autos.all.count).to eq(1)
   end
 
+  it 'Debería asignar un id' do
+    nuevo_auto = repo_autos.save(auto)
+    expect(nuevo_auto.id).to be_present
+  end
+
+  context 'Cuando un auto existe' do
+    before :each do
+      @nuevo_auto = repo_autos.save(auto)
+      @id_auto = @nuevo_auto.id
+    end
+
+    it 'No debería tener autos al eliminar todos' do
+      repo_autos.delete_all
+      expect(repo_autos.all.count).to eq(0)
+    end
+
+    it 'Deber encontrar por el id' do
+      auto = repo_autos.find(@id_auto)
+      expect(auto.id).to eq(@nuevo_auto.id)
+    end
+  end
+
+  it 'Debería lanzar un error cuando el auto no existe' do
+    expect do
+      repo_autos.find(99999)
+    end.to raise_error(ObjectNotFound)
+  end
 end
