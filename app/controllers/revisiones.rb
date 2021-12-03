d19e9fefa43cff8c0f76cf89acfca2aa4ae86e20
lotero1 <lotero@fi.uba.ar>
2021-12-02 23:19:57 -0300

    ops: Agrega logger para poder ver errores en el endpoint de cotizacion
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

--- app/controllers/revisiones.rb
+++ app/controllers/revisiones.rb
@@ -7,6 +7,7 @@ WebTemplate::App.controllers :revisiones, :provides => [:json] do
       status 201
       {mensaje: 'revisión exitosa'}.to_json
     rescue Exception => e
+      logger.info e.backtrace
       status 400
       {mensaje: 'se produjo un error'}.to_json
     end
