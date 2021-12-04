9d26e1ecddf688adb41354b898eb36d93807ab42
lotero1 <lotero@fi.uba.ar>
2021-12-04 15:27:52 -0300

    feat: Agrego steps y descomento el feature de publicacion por p2p
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

---
+++ features/step_definitions/publicacion_steps.rb
@@ -0,0 +1,15 @@
+Cuando('rechazo la cotización de Fiubak y publico por p2p con precio {int}') do |precio|
+    body = {
+    'id_intencion_de_venta': @id_intencion,
+    'precio': precio
+  }
+  @response = Faraday.post(crear_publicaciones, body.to_json, header)
+  data = JSON.parse(@response.body)
+  @id_publicacion = data['valor']['id_publicacion']
+end
+
+Entonces('veo el auto publicado con id correspondiente para venta a un valor de {int}') do |precio|
+    @response = Faraday.get(obtener_publicaciones)
+    publicaciones = JSON.parse(@response.body)
+    expect(publicaciones.any?{ |publicacion| publicacion['id'] == @id_publicacion && publicacion['precio'] == precio}).to eq(true)
+  end
