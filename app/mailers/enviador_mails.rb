213dd5f0c42473b736bfbbb9d815419fede229ce
delpinor <delpinor@gmail.com>
2021-12-04 12:12:31 -0300

    feat: cambio minimo en la plantilla del email

--- app/mailers/enviador_mails.rb
+++ app/mailers/enviador_mails.rb
@@ -4,13 +4,25 @@ class EnviadorMails
         patente = intencion_de_venta.auto.patente
         precio = revision.valor_cotizado
 
-        enviar_mail(email, 'revision recibida', cuerpo_cotizacion(intencion_de_venta.id, patente, precio))
+        enviar_mail(email, 'Cotización', cuerpo_cotizacion(intencion_de_venta.id, patente, precio))
     end
 
     private
 
     def cuerpo_cotizacion(id_intencion_de_venta, patente, precio)
-        "Se registro la cotizacion de la intencion de venta con id: #{id_intencion_de_venta}, precio #{precio} para el auto con patente: #{patente}"
+      titulo = 'Cotización FIUBAK' + "\n" + "\n"
+      cuerpo = 'A continuación le enviamos la cotización:' + "\n"
+      "Id. de intención de venta: #{id_intencion_de_venta} " + "\n"
+        "Precio cotizado en pesos($): #{precio}" + "\n"
+        "Patente del auto cotizado: #{patente}" + "\n"
+
+      pie = 'Si desea aceptar la cotización en el bot debe ingresar:' + "\n"
+        "/aceptar_cotización #{id_intencion_de_venta}" + "\n" + "\n"
+
+      saludos = 'Saludos, ' + "\n" + "\n"
+        'Equipo FIUBAK'
+      
+      titulo + cuerpo + pie + saludos
     end
 
     def enviar_mail(email, asunto, cuerpo)
