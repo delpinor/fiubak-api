1c2d166257eea03a0c2f7f39103b32bda1dc2b38
lotero1 <lotero@fi.uba.ar>
2021-12-06 11:49:14 -0300

    fix: Arreglo tests de aceptacion de cotizacion a intencion de venta inexistente
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

--- features/step_definitions/cotizacion_inexistente_steps.rb
+++ features/step_definitions/cotizacion_inexistente_steps.rb
@@ -1,5 +1,5 @@
 Cuando('intento aceptar la cotización de id {int} de Fiubak') do |id_consulta|
   datos = {id_intencion: id_consulta}
-  @response = put('/aceptar_cotizacion', datos.to_json, { 'CONTENT_TYPE' => 'application/json' })
-  expect(last_response.status).to eq(404)
+  @response = Faraday.put(aceptar_cotizacion_url, datos.to_json, header)
+  expect(@response.status).to eq(404)
 end
