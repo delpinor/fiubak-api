b87c99fb68085a727a5e3ce3213f525e58fb1418
lotero1 <lotero@fi.uba.ar>
2021-12-06 18:53:19 -0300

    test: Agrego test del campo estado para la entidad Oferta
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

--- app/models/oferta.rb
+++ app/models/oferta.rb
@@ -1,15 +1,17 @@
 class Oferta
-  attr_reader :usuario, :valor, :id_publicacion
+  attr_reader :usuario, :valor, :id_publicacion, :estado
   attr_accessor :id
 
-  def initialize(usuario, valor, id = nil, id_publicacion = nil)
+  def initialize(usuario, valor, estado, id = nil, id_publicacion = nil)
     @usuario = usuario
     @valor = valor
     @id_publicacion = id_publicacion
+    @estado = estado
     validar_oferta
   end
 
   def validar_oferta
+    #Validar estados: creada, aceptada, rechazada
     nil
   end
 end
--- spec/app/models/oferta_spec.rb
+++ spec/app/models/oferta_spec.rb
@@ -7,7 +7,7 @@ describe 'Crear oferta' do
       @usuario_comprador = Usuario.new(33234543, 'Nicolas', 'nicoperez@gmail.com', 665)
       @usuario_vendedor = Usuario.new(332345431, 'Nicolas2', 'nicopere2z@gmail.com', 6635)
       @publicacion = Publicacion.new(@usuario_vendedor, @auto, 75000, "Fiubak",1)
-      @oferta = Oferta.new(@usuario_comprador, 75000)
+      @oferta = Oferta.new(@usuario_comprador, 75000, "Creada")
     end
     it 'Se crea con usuario comprador' do
       expect(@oferta.usuario.nombre).to eq "Nicolas"
@@ -15,6 +15,10 @@ describe 'Crear oferta' do
 
     it 'Se crea con valor' do
       expect(@oferta.valor).to eq 75000
+    end
+
+    it 'Se crea con estado' do
+      expect(@oferta.estado).to eq 'Creada'
     end
 
   end
