1c803a02671857042ae7b656fa98247adc4a558c
JuanOpizzi <juanopizzi@gmail.com>
2021-12-11 15:51:22 -0300

    test: agrego manejo de error de mail duplicado

--- app/controllers/usuarios.rb
+++ app/controllers/usuarios.rb
@@ -2,9 +2,13 @@ WebTemplate::App.controllers :usuarios, :provides => [:json] do
   post :create, :map => '/usuarios' do
     begin
       nuevo_usuario = crear_usuario(request.body.read)
+      repositorio_de_usuarios.find_by_email(nuevo_usuario.email)
       usuario_con_id = repositorio_de_usuarios.save(nuevo_usuario)
       status 201
       {mensaje: "Registro exitoso bajo id: #{usuario_con_id.id}"}.to_json
+    rescue MailYaRegistradoError => e
+      status 404
+      {mensaje: 'El email ya se encuentra registrado'}.to_json
     rescue Exception => e
       status 400
       {mensaje: 'se produjo un error'}.to_json
--- spec/app/controllers/usuarios_controller_spec.rb
+++ spec/app/controllers/usuarios_controller_spec.rb
@@ -4,7 +4,8 @@ describe 'Usuarios controller' do
 
   let(:repo_usuario) { Persistence::Repositories::RepositorioDeUsuarios.new }
   let(:datos_usuario) { {dni: 12778523, nombre: "pepe", email: "pepe@gmail.com", id: 343} }
-  let(:datos_usuario2) { {dni: 12778523, nombre: "pepe", email: "pepe@gmail.com", id: 3443} }
+  let(:datos_usuario2) { {dni: 12778523, nombre: "pepe", email: "mama@gmail.com", id: 3443} }
+  let(:datos_usuario3) { {dni: 12779999, nombre: "lucas", email: "pepe@gmail.com", id: 3443} }
 
   it 'La respuesta debe ser un mensaje exitoso' do
     post('/usuarios', datos_usuario.to_json, { 'CONTENT_TYPE' => 'application/json' })
@@ -17,6 +18,13 @@ describe 'Usuarios controller' do
     post('/usuarios', datos_usuario2.to_json, { 'CONTENT_TYPE' => 'application/json' })
     body = JSON.parse(last_response.body)
     expect(body['mensaje']).to eq('se produjo un error')
+  end
+
+  it 'La respuesta debe ser un mensaje de email duplicado al cargar email duplicado' do
+    post('/usuarios', datos_usuario.to_json, { 'CONTENT_TYPE' => 'application/json' })
+    post('/usuarios', datos_usuario3.to_json, { 'CONTENT_TYPE' => 'application/json' })
+    body = JSON.parse(last_response.body)
+    expect(body['mensaje']).to eq('El email ya se encuentra registrado')
   end
 
   it 'El estado debe ser 201 al crear correctamente' do
