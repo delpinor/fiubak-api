5ad211932e68dfac2216971f529ddd625da3798b
delpinor <delpinor@gmail.com>
2021-12-11 20:09:15 -0300

    fix: falla por test que valida la creacion del una variable de entorno. Se elimina

--- app/models/errors/variables_no_creadas_error.rb
+++
@@ -1,2 +0,0 @@
-class VariablesNoCreadasError < StandardError
-end
--- app/models/validador_de_token.rb
+++ app/models/validador_de_token.rb
@@ -1,24 +1,19 @@
 require_relative '../../app/models/errors/no_autorizado_error'
-require_relative '../../app/models/errors/variables_no_creadas_error'
+require_relative '../../app/models/errors/variables_no_definidas_error'
 
 class ValidadorDeToken
   def initialize
     @token_bot = ENV['HTTP_BOT_TOKEN']
     @token_rev = ENV['HTTP_REV_TOKEN']
-    validar_que_existan
   end
 
   def validar_para_revision(token)
+    raise VariablesNoDefinidasError if token == nil
     raise NoAutorizadoError if @token_rev != token
   end
 
   def validar_para_bot(token)
+    raise VariablesNoDefinidasError if token == nil
     raise NoAutorizadoError if @token_bot != token
-  end
-
-  private
-
-  def validar_que_existan
-    raise VariablesNoCreadasError if @token_rev == nil or @token_bot == nil
   end
 end
