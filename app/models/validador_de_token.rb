a413e41d6be93aebb87da530b73c5644560ed917
delpinor <delpinor@gmail.com>
2021-12-11 13:21:43 -0300

    refactor: se cambiaron los nombres de los metodos de ValidadorDeToken para dar mayor claridad

--- app/models/validador_de_token.rb
+++ app/models/validador_de_token.rb
@@ -1,9 +1,14 @@
 class ValidadorDeToken
   def initialize
-    @token = ENV['HTTP_BOT_TOKEN']
+    @token_bot = ENV['HTTP_BOT_TOKEN']
+    @token_rev = ENV['HTTP_REV_TOKEN']
   end
 
-  def es_valido?(token)
-    true
+  def valido_para_revision?(token)
+    @token_rev == token
+  end
+
+  def valido_para_bot?(token)
+    @token_bot == token
   end
 end
--- spec/app/models/validador_de_token_spec.rb
+++ spec/app/models/validador_de_token_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
-describe 'Validador por token de los endpoint' do
+describe 'Validador por token' do
 
   let(:validador){ValidadorDeToken.new}
 
-  it 'Cuando valido con un token incorrecto entonces no estoy autorizado' do
-    expect(validador.es_valido?('safasdfsd')).to eq(true)
+  it 'Cuando es invalido para bot' do
+    expect(validador.valido_para_bot?('safasdfsd')).to eq(false)
   end
 
-  xit 'Cuando valido con un token incorrecto entonces no estoy autorizado' do
-    expect(validador.es_valido?(ENV['HTTP_BOT_TOKEN'])).to eq(true)
+  it 'Cuando es invalido para bot' do
+    expect(validador.valido_para_revision?('safasdfsd')).to eq(false)
+  end
 
+  it 'Cuando es valido' do
+    expect(validador.valido_para_bot?(ENV['HTTP_BOT_TOKEN'])).to eq(true)
+  end
+
+  it 'Cuando es valido' do
+    expect(validador.valido_para_revision?(ENV['HTTP_REV_TOKEN'])).to eq(true)
   end
 end
