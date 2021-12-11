8fee38ac48583fe2ea7d09201fdd3057a28584f9
JuanOpizzi <juanopizzi@gmail.com>
2021-12-11 16:53:33 -0300

    test: agrego test unitario de mail duplicado pero con letras capitales

--- spec/app/controllers/usuarios_controller_spec.rb
+++ spec/app/controllers/usuarios_controller_spec.rb
@@ -6,6 +6,7 @@ describe 'Usuarios controller' do
   let(:datos_usuario) { {dni: 12778523, nombre: "pepe", email: "pepe@gmail.com", id: 343} }
   let(:datos_usuario2) { {dni: 12778523, nombre: "pepe", email: "mama@gmail.com", id: 3443} }
   let(:datos_usuario3) { {dni: 12779999, nombre: "lucas", email: "pepe@gmail.com", id: 3443} }
+  let(:datos_usuario4) { {dni: 12779999, nombre: "gaston", email: "PEPE@gmail.com", id: 3443} }
 
   it 'La respuesta debe ser un mensaje exitoso' do
     post('/usuarios', datos_usuario.to_json, header_con_token)
@@ -23,6 +24,13 @@ describe 'Usuarios controller' do
   it 'La respuesta debe ser un mensaje de email duplicado al cargar email duplicado' do
     post('/usuarios', datos_usuario.to_json, header_con_token)
     post('/usuarios', datos_usuario3.to_json, header_con_token)
+    body = JSON.parse(last_response.body)
+    expect(body['mensaje']).to eq('El email ya se encuentra registrado')
+  end
+
+  it 'La respuesta debe ser email duplicado al cargar email duplicado aunque se diferencie por letras capitales' do
+    post('/usuarios', datos_usuario3.to_json, header_con_token)
+    post('/usuarios', datos_usuario4.to_json, header_con_token)
     body = JSON.parse(last_response.body)
     expect(body['mensaje']).to eq('El email ya se encuentra registrado')
   end
